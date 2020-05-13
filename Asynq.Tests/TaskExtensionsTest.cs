28795acf63b2465b66e3fe9eb42e25893dfd04d0
Ygor Henrique Sobral Randes <ysobralrandes@travix.com>
2020-05-13 20:19:03 +0200

    Added AsValueTask extension

---
+++ Asynq.Tests/TaskExtensionsTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Asynq.Tests
+{
+    public class TaskExtensionsTest
+    {
+        [Fact]
+        public async Task GenericAsTask_ValueTaskHoldsOriginalTask()
+        {
+            Task<int> asyncVal = AsyncValue(100);
+
+            ValueTask<int> valueTask = asyncVal.AsValueTask();
+
+            Assert.Equal(asyncVal, valueTask.AsTask());
+            Assert.False(valueTask.IsCompletedSuccessfully);
+            Assert.Equal(100, await valueTask);
+        }
+
+        [Fact]
+        public async Task NonGenericAsTask_ReturnsValidValueTask()
+        {
+            Task asyncTask = Task.Delay(10);
+            ValueTask valueTask = asyncTask.AsValueTask();
+
+            Assert.Equal(asyncTask, valueTask.AsTask());
+            Assert.False(valueTask.IsCompletedSuccessfully);
+            await valueTask;
+            Assert.True(valueTask.IsCompletedSuccessfully);
+        }
+
+        public async Task<T> AsyncValue<T>(T value)
+        {
+            await Task.Delay(10);
+            return value;
+        }
+    }
+}
---
+++ Asynq/TaskExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace Asynq
+{
+    public static class TaskExtensions
+    {
+        public static ValueTask AsValueTask(this Task task)
+        {
+            return new ValueTask(task);
+        }
+
+        public static ValueTask<T> AsValueTask<T>(this Task<T> task)
+        {
+            return new ValueTask<T>(task);
+        }
+    }
+}
