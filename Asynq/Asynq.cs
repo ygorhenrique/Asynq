fe193022052fcacbd803a5fe9806f5ee55db82d3
Ygor Henrique Sobral Randes <rick.ygor@live.com>
2020-07-09 12:36:01 +0200

    Added exception filter to FireAndForget

--- Asynq/Asynq.cs
+++ Asynq/Asynq.cs
@@ -5,29 +5,41 @@ namespace Asynq
 {
     public static class Asynq
     {
+        /// <summary>
+        /// Returns all values from each ValueTask as an array
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="valueTasks"></param>
+        /// <returns>Array of values when all ValueTasks have been completed</returns>
         public static ValueTask<T[]> WhenAll<T>(params ValueTask<T>[] valueTasks)
         {
             return valueTasks.WhenAll();
         }
 
+        /// <summary>
+        /// Returns any of the completed ValueTasks
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="valueTasks"></param>
+        /// <returns>One of the completed ValueTasks</returns>
         public static ValueTask<T> WhenAny<T>(params ValueTask<T>[] valueTasks)
         {
             return valueTasks.WhenAny();
         }
 
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="valueTask"></param>
+        /// <param name="onException"></param>
         public static async void FireAndForget(this ValueTask valueTask, Action<Exception> onException = null)
         {
             try
             {
                 await valueTask.ConfigureAwait(false);
             }
-            catch (Exception e)
+            catch (Exception e) when (onException != null)
             {
-                if (onException == null)
-                {
-                    throw;
-                }
-
                 onException(e);
             }
         }
